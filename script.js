e07ae93c9a231d236817d6bf41c73f0ee14aecdd
Liza <lizakorneva1997@gmail.com>
2022-02-23 15:51:30 +0300

    added solo game

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const cells = document.querySelectorAll('.cell');
 const btn = document.querySelector('.btn-reset');
 const whoseTurn = document.querySelector('.whose-turn');
 
+const main = document.querySelector('.main');
+const title = document.querySelector('.title');
+
+let gameVariant; // Определяет режим игры
+const startBackground = document.querySelector('.start-background');
+const twoPlayers = document.querySelector('.two-players');
+const soloGame = document.querySelector('.solo-game');
+
 let move = 'cross'; // Ход игрока Х
 let arrValueCross = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 let arrValueZero = [0, 0, 0, 0, 0, 0, 0, 0, 0];
@@ -11,6 +19,22 @@ const arrOfCorrectResults = [ // Массив выигрышных ходов
   [0, 0, 0, 0, 0, 0, 1, 1, 1], [1, 0, 0, 0, 1, 0, 0, 0, 1], [0, 0, 1, 0, 1, 0, 1, 0, 0],
 ];
 
+// Выбрать режим и начать игру
+function soloOrTwoPlayers(event) {
+  if (event.target.innerText === 'play alone') {
+    gameVariant = 'solo';
+    whoseTurn.style.display = 'none';
+  } else {
+    gameVariant = 'two players';
+    whoseTurn.style.display = 'block';
+  }
+  startBackground.style.display = 'none';
+  twoPlayers.style.display = 'none';
+  soloGame.style.display = 'none';
+  main.style.opacity = '1';
+  title.style.opacity = '1';
+}
+
 // Обновляет данные по нажатию кнопки reset
 function dataUpdate() {
   cells.forEach((cell) => {
@@ -18,9 +42,27 @@ function dataUpdate() {
     cell.classList.remove('zero');
   });
   move = 'cross';
-  whoseTurn.innerText = 'Turn X';
+  if (gameVariant === 'solo') {
+    whoseTurn.style.display = 'none';
+  } else {
+    whoseTurn.innerText = 'Turn X';
+  }
   arrValueCross = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   arrValueZero = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+}
+
+// Проверяет остались ли свободные ячейки
+function checkFreeCell() {
+  let freeCell = 0;
+  for (let i = 0; i < cells.length; i += 1) {
+    if (cells[i].classList.contains('cross') !== true && cells[i].classList.contains('zero') !== true) {
+      freeCell += 1;
+    }
+  }
+  if (freeCell > 0) {
+    return true;
+  }
+  return false;
 }
 
 // Проверяет все ли ячейки заполнены классами
@@ -73,6 +115,9 @@ function classСheck(cell) {
 // Добавление hover класса
 function mouseoverOnElem(event) {
   const cell = event.target;
+  if (gameVariant === 'solo' && move === 'zero') {
+    return;
+  }
   if (classСheck(cell)) {
     return;
   }
@@ -86,6 +131,9 @@ function mouseoverOnElem(event) {
 // Удаление hover класса
 function mouseoutElem(event) {
   const cell = event.target;
+  if (gameVariant === 'solo' && move === 'zero') {
+    return;
+  }
   if (classСheck(cell)) {
     return;
   }
@@ -98,9 +146,11 @@ function mouseoutElem(event) {
 
 // Выводит окошко конца игры
 function messageAboutWin(msg) {
-  whoseTurn.innerText = '';
-  const main = document.querySelector('.main');
-  const title = document.querySelector('.title');
+  if (gameVariant === 'solo') {
+    whoseTurn.style.display = 'none';
+  } else {
+    whoseTurn.innerText = '';
+  }
   const winBackground = document.querySelector('.win-background');
   const winBtn = document.querySelector('.win-btn');
   const winMessage = document.querySelector('.win-message');
@@ -116,44 +166,97 @@ function messageAboutWin(msg) {
     title.style.opacity = '1';
     winBackground.style.display = 'none';
     dataUpdate();
+    startBackground.style.display = 'flex';
+    twoPlayers.style.display = 'block';
+    soloGame.style.display = 'block';
+    main.style.opacity = '.1';
+    title.style.opacity = '.1';
+    whoseTurn.style.display = 'none';
   });
+}
+
+// Поиск рандомной ячейки
+function searchRandomCell() {
+  let randomCell = Math.floor(Math.random() * 9);
+  while (cells[randomCell].classList.contains('cross') || cells[randomCell].classList.contains('zero')) {
+    randomCell = Math.floor(Math.random() * 9);
+  }
+  return randomCell;
 }
 
 // Добавление cross или zero по клику
 function clickOnElem(event) {
   const cell = event.target;
-  if (classСheck(cell)) {
-    return;
+  if (gameVariant === 'two players') {
+    if (move === 'cross') {
+      whoseTurn.innerText = 'Turn 0';
+      cell.classList.add('cross');
+      cell.classList.remove('hover-cross');
+      arrValueCross[cell.dataset.cell] = 1;
+      if (compareArrayValues() === 'win') {
+        messageAboutWin('Win X!!!');
+      } else if (compareArrayValues()) {
+        messageAboutWin("It's a draw!");
+      }
+      move = 'zero';
+    } else {
+      whoseTurn.innerText = 'Turn X';
+      cell.classList.add('zero');
+      cell.classList.remove('hover-zero');
+      arrValueZero[cell.dataset.cell] = 1;
+      if (compareArrayValues() === 'win') {
+        messageAboutWin('Win 0!!!');
+      } else if (compareArrayValues()) {
+        messageAboutWin("It's a draw!");
+      }
+      move = 'cross';
+    }
   }
-  if (move === 'cross') {
-    whoseTurn.innerText = 'Turn 0';
-    cell.classList.add('cross');
-    cell.classList.remove('hover-cross');
-    arrValueCross[cell.dataset.cell] = 1;
-    if (compareArrayValues() === 'win') {
-      messageAboutWin('Win X!!!');
-    } else if (compareArrayValues()) {
-      messageAboutWin("It's a draw!");
+  if (gameVariant === 'solo') {
+    if (move === 'cross') {
+      cell.classList.add('cross');
+      cell.classList.remove('hover-cross');
+      arrValueCross[cell.dataset.cell] = 1;
+      if (compareArrayValues() === 'win') {
+        messageAboutWin('Win X!!!');
+      }
+      if (checkFreeCell() !== true) {
+        if (compareArrayValues() === 'win') {
+          messageAboutWin('Win X!!!');
+          return;
+        }
+        if (compareArrayValues()) {
+          messageAboutWin("It's a draw!");
+          return;
+        }
+      }
+      move = 'zero';
+      // Ход компьютера
+      const random = searchRandomCell();
+      cells[random].classList.add('zero');
+      arrValueZero[random] = 1;
+      if (compareArrayValues() === 'win') {
+        messageAboutWin('Win 0!!!');
+      }
+      if (checkFreeCell() !== true) {
+        if (compareArrayValues() === 'win') {
+          messageAboutWin('Win 0!!!');
+        } else if (compareArrayValues()) {
+          messageAboutWin("It's a draw!");
+        }
+      }
+      move = 'cross';
     }
-    move = 'zero';
-  } else {
-    whoseTurn.innerText = 'Turn X';
-    cell.classList.add('zero');
-    cell.classList.remove('hover-zero');
-    arrValueZero[cell.dataset.cell] = 1;
-    if (compareArrayValues() === 'win') {
-      messageAboutWin('Win 0!!!');
-    } else if (compareArrayValues()) {
-      messageAboutWin("It's a draw!");
-    }
-    move = 'cross';
   }
 }
 
 cells.forEach((cell) => {
-  cell.addEventListener('mouseover', mouseoverOnElem); // Наведении мышки на Х
+  cell.addEventListener('mouseover', mouseoverOnElem); // Наведении мышки на ячейку
   cell.addEventListener('mouseout', mouseoutElem); // Отведение мышки
-  cell.addEventListener('click', clickOnElem); // Клик X
+  cell.addEventListener('click', clickOnElem); // Клик по ячейке
 });
 
 btn.addEventListener('click', dataUpdate); // Событие по клику на кнопку
+
+soloGame.addEventListener('click', soloOrTwoPlayers);
+twoPlayers.addEventListener('click', soloOrTwoPlayers);
